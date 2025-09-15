b634a6fdfca25e122fb41efcd53664eee8711890
Justin Willoughby <justin@clerk.dev>
2025-09-15 11:53:26 -0500

    Add user button to favorites page to do sign outs quickly

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -1,7 +1,12 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+
 export default function Favorites() {
   return (
     <>
       <h1>Favorites</h1>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </>
   )
 }
