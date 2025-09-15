a326a253385d2bf95c3d720452585eab5ecd3bdb
Justin Willoughby <justin@clerk.dev>
2025-09-15 11:56:28 -0500

    Add favorites link to homepage for testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
+      <Link href="/favorites">Favorites</Link>
       <SignedOut>
         <SignInButton />
         <SignUpButton />
